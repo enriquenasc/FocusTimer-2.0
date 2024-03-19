1cb3cbf95f9f8d108db837ace901f00bff38d598
Enrique Nascimento <enriquean30@gmail.com>
2024-03-19 11:32:12 -0300

    Sounds

--- js/sounds.js
+++ js/sounds.js
@@ -3,7 +3,7 @@ export default function () {
     'https://github.com/maykbrito/automatic-video-creator/blob/master/audios/button-press.wav?raw=true'
   )
   const kitchenTimer = new Audio(
-    ''
+    'https://github.com/maykbrito/automatic-video-creator/blob/master/audios/kichen-timer.mp3?raw=true'
   )
   const forest = new Audio(
     ''

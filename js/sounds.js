69615bd5f9ad0098d2c0793b583e07a9ebe1c968
Enrique Nascimento <enriquean30@gmail.com>
2024-03-19 11:35:10 -0300

    Sounds

--- js/sounds.js
+++ js/sounds.js
@@ -1,61 +1,53 @@
 export default function () {
   const buttonPressAudio = new Audio(
-    'https://github.com/maykbrito/automatic-video-creator/blob/master/audios/button-press.wav?raw=true'
-  )
+    "https://github.com/maykbrito/automatic-video-creator/blob/master/audios/button-press.wav?raw=true"
+  );
   const kitchenTimer = new Audio(
-    'https://github.com/maykbrito/automatic-video-creator/blob/master/audios/kichen-timer.mp3?raw=true'
-  )
-  const forest = new Audio(
-    ''
-  )
-  const cloudy = new Audio(
-    ''
-  )
-  const coffee = new Audio(
-    ''
-  )
-  const flame = new Audio(
-    ''
-  )
-  let musics = { forest, cloudy, coffee, flame }
+    "https://github.com/maykbrito/automatic-video-creator/blob/master/audios/kichen-timer.mp3?raw=true"
+  );
+  const forest = new Audio("./Sounds/Floresta.wav");
+  const cloudy = new Audio("./Sounds/Chuva.wav");
+  const coffee = new Audio("./Sounds/Cafeteria.wav");
+  const flame = new Audio("./Sounds/Lareira.wav");
+  let musics = { forest, cloudy, coffee, flame };
 
-  musics.forest.loop = true
-  musics.cloudy.loop = true
-  musics.coffee.loop = true
-  musics.flame.loop = true
+  musics.forest.loop = true;
+  musics.cloudy.loop = true;
+  musics.coffee.loop = true;
+  musics.flame.loop = true;
 
   function pressButton() {
-    buttonPressAudio.play()
+    buttonPressAudio.play();
   }
 
   function timeEnd() {
-    kitchenTimer.play()
+    kitchenTimer.play();
   }
 
   function forestMusicOn() {
-    musics.forest.play()
-    musics.cloudy.pause()
-    musics.coffee.pause()
-    musics.flame.pause()
+    musics.forest.play();
+    musics.cloudy.pause();
+    musics.coffee.pause();
+    musics.flame.pause();
   }
- 
+
   function cloudyMusicOn() {
-    musics.forest.pause()
-    musics.cloudy.play()
-    musics.coffee.pause()
-    musics.flame.pause()
+    musics.forest.pause();
+    musics.cloudy.play();
+    musics.coffee.pause();
+    musics.flame.pause();
   }
   function coffeeMusicOn() {
-    musics.forest.pause()
-    musics.cloudy.pause()
-    musics.coffee.play()
-    musics.flame.pause()
+    musics.forest.pause();
+    musics.cloudy.pause();
+    musics.coffee.play();
+    musics.flame.pause();
   }
   function flameMusicOn() {
-    musics.forest.pause()
-    musics.cloudy.pause()
-    musics.coffee.pause()
-    musics.flame.play()
+    musics.forest.pause();
+    musics.cloudy.pause();
+    musics.coffee.pause();
+    musics.flame.play();
   }
 
   return {
@@ -65,8 +57,7 @@ export default function () {
     cloudyMusicOn,
     coffeeMusicOn,
     flameMusicOn,
-
-  }
+  };
 }
 /*
   function playAudio(music, musics){
